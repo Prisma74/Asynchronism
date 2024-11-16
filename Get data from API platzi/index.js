1ff0752b3291a88f5209b4ee67b158a5cf76da97
Prisma74 <solopipe2001@gmail.com>
2024-11-15 19:44:49 -0500

    index.js init (not finished)

---
+++ Get data from API platzi/index.js
@@ -0,0 +1,16 @@
+function fetchProducts() {
+  return new Promise((resolve, reject) => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Error getting products");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        resolve(data);
+      }).catch((error) => {
+        reject()
+      })
+  });
+}
